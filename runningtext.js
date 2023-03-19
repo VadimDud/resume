df5896c0c14d64b7fd2334fcf8448a4a5d7024b7
VadimDud <109547721+VadimDud@users.noreply.github.com>
2023-03-19 11:33:13 +0300

    Create runningtext.js

---
+++ runningtext.js
@@ -0,0 +1,11 @@
+const text = "HTML, CSS, Python, SQL, JS, Selenium Webdriver with Python, Qase, Jira, TestRail, Figma, Invision, Photoshop, Windows, MAC, iOS, Android, Chrome, Safari, FireFox, Opera, Tor Browser, Edge, PostgreSQL, MS SQL Server, MS Access, Slack, Github, ADB, Postman";
+const runningText = document.getElementById("running-textt");
+let index = 0;
+
+setInterval(function() {
+  runningText.textContent = text.slice(0, index);
+  index++;
+  if (index > text.length) {
+    index = 0;
+  }
+}, 40);
